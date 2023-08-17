ca2eb4bc5cc1d185c6bfcc73f3e64ed8b8d0cd31
So Awesome Man <soawesomeman@pm.me>
2023-08-16 18:36:38 -0700

    feature: frontend cache results useEffect + localstorage

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -4,12 +4,19 @@ function App() {
     //const { search, animals } = useAnimalSearch();
     const [animals, setAnimals] = useState([]);
 
+    useEffect(() => {
+        const lastQuery = localStorage.getItem('lastQuery');
+        search(lastQuery);
+    }, []);
+
     const search = async (q) => {
         const response = await fetch(
             'http://localhost:8080?' + new URLSearchParams({ q })
         );
         const data = await response.json();
         setAnimals(data);
+
+        localStorage.setItem('lastQuery', q);
     };
 
     return (
